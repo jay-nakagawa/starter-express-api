e2dbf6c9e7e75db03a53507e55d496c4a1a0ad37
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2022-12-09 16:00:23 -0500

    added additional files and a listen cb

--- index.js
+++ index.js
@@ -4,13 +4,47 @@ const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 3000
 
-// mongoose.connect("")
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect("mongodb+srv://jaynakagawa777:mongod123@cluster0.t6yfhjw.mongodb.net/?retryWrites=true&w=majority");
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+      } catch (error) {
+        console.log(error);
+        process.exit(1);
+      }
+    }
+
+
+
+// mongoose.connect("mongodb+srv://jaynakagawa777:mongod123@cluster0.t6yfhjw.mongodb.net/?retryWrites=true&w=majority" , ()=> {
+
+// console.log("Connected to DB")
+// })
+
+
+// app.set('view engine', 'ejs')
+// app.use(express.static('public'))
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.json())
+
+// let db = 'test'
+
+// app.get('/',(request, response)=>{
+//     db.collection('rappers').find().sort({likes: -1}).toArray()
+//     .then(data => {
+//         response.render('index.ejs', { info: data })
+//     })
+//     .catch(error => console.error(error))
+// })
 
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('hi!')
 })
-app.listen(PORT,() => {
-    console.log(`Listening on port ${PORT}`)
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
 })
