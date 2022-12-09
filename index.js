ddba810d16640fd45453d68a37494baea40bfd08
Justin Dickson <112723736+jay-nakagawa@users.noreply.github.com>
2022-12-09 15:37:58 -0500

    added port variable and mongoose

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT || 3000
+
+// mongoose.connect("")
+
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('hi!')
 })
-app.listen(process.env.PORT || 3000)
+app.listen(PORT,() => {
+    console.log(`Listening on port ${PORT}`)
+})
